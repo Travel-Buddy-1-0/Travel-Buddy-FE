a9d355401faef6b09a3c4abed255765fd36b0abf
HungHQ <142767137+MeoSuytGay@users.noreply.github.com>
2025-10-08 00:42:06 +0700

    update

--- src/services/Authen/getGoogleLoginUrl.js
+++ src/services/Authen/getGoogleLoginUrl.js
@@ -1,6 +1,6 @@
 export async function getGoogleLoginUrl() {
   try {
-    const res = await fetch("hhttps://travel-buddy-web.azurewebsites.net/Authentication/login-google");
+    const res = await fetch("https://travel-buddy-web.azurewebsites.net/Authentication/login-google");
     if (!res.ok) throw new Error("Failed to get Google login URL");
  
     const url = await res.text(); // backend trả về URL redirect
